494b388e20642909dd03ffa57d3a612d9d5d2cd6
Wes <wes.martin17@gmail.com>
2020-02-08 17:29:27 -0500

    pls work

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from papirus import PapirusTextPos
-from ui.Item_List import Item_List
+import ui.Item_List as IL
 
 
 def main():
@@ -11,22 +11,12 @@ def main():
             papirus.clear()
             sys.exit()
         if GPIO.input(SW4) == False:
-            draw_text("button 4 pressed")
+            print("button 4 pressed")
         if GPIO.input(SW3) == False:
-            draw_text("button 3 pressed")
+            print("button 3 pressed")
         if GPIO.input(SW1) == False:
-            Item_List().draw_lines_and_text(["Hello", "world", "dog"], 3)
+            IL.Item_List().draw_lines_and_text(["Hello", "world", "dog"], 3)
         sleep(0.1)
-
-
-def draw_text(text_str):
-
-    # Same as calling "PapirusTextPos(True [,rotation = rot])"
-    text = PapirusTextPos([rotation=rot])
-
-    # Write text to the screen at selected point, with an Id
-    # "hello world" will appear on the screen at (10, 10), font size 20, straight away
-    text.AddText(text_str, 0, 10, Id="Start")
 
 
 main()
