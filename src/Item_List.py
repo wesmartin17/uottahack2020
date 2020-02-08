388f89d3fad346d5c974f27993db197c30757563
Wes <wes.martin17@gmail.com>
2020-02-08 18:27:57 -0500

    rly?

--- src/Item_List.py
+++ src/Item_List.py
@@ -4,16 +4,19 @@ from papirus import Papirus, PapirusComposite
 class Item_List():
 
     items = []
+    items_per_screen = 0
     textNImg = PapirusComposite(False)
     selected = 0
 
     def __init__(self, items, items_per_screen=3):
         self.items = items
-        self.num_lines = items_per_screen
+        self.items_per_screen = items_per_screen
         self.draw_lines_and_text(items, items_per_screen)
 
-    def draw_lines_and_text(self, items, num_lines):
+    def draw_lines_and_text(self):
         horiz_line = "./img/ui/horiz_line.bmp"
+        num_lines = self.items_per_screen
+        items = self.items
         # LINES
         for i in range(num_lines):
             h = 128/num_lines * (i+1)
@@ -26,16 +29,16 @@ class Item_List():
                 invert = True
             h = (128/num_lines * i) + 10
             self.textNImg.AddText(items[i], 10, h, Id=i, invert=invert)
-
-    def show(self):
         self.textNImg.WriteAll()
 
     def select_up(self):
         self.selected = self.selected - 1
         if self.selected < 0:
             self.selected = len(self.items)-1
+        self.draw_lines_and_text()
 
     def select_down(self):
         self.selected = self.selected + 1
         if self.selected > len(self.items):
             self.selected = 0
+        self.draw_lines_and_text()
