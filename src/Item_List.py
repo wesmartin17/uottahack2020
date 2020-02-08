c270f3e4041be2aa9b804c4a8b3098526914f41c
Wes <wes.martin17@gmail.com>
2020-02-08 17:57:48 -0500

    bruh

--- src/Item_List.py
+++ src/Item_List.py
@@ -9,9 +9,9 @@ class Item_List():
     def __init__(self, items, items_per_screen=3):
         self.items = items
         self.num_lines = items_per_screen
-        self.draw_lines_and_text(items_per_screen)
+        self.draw_lines_and_text(items, items_per_screen)
 
-    def draw_lines_and_text(self, num_lines):
+    def draw_lines_and_text(self, items, num_lines):
         horiz_line = "./img/ui/horiz_line.bmp"
 
         for i in range(num_lines):
@@ -19,7 +19,7 @@ class Item_List():
             self.textNImg.AddImg(horiz_line, 0, h,
                                  Id="Line{}".format(str(i)))
 
-        for i in self.items:
+        for i in items:
             h = 128/num_lines * i
             self.textNImg.AddText(i, 0, h, Id=i)
 
