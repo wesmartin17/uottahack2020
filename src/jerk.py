428d65a8980c9dcd81eaf43790d61ca1e9c5cea3
KeenanNodam <keenan.muk@gmail.com>
2020-02-09 03:02:26 -0500

    This includes a simple structure for the class, along with the comments describing some of the functions involved.

--- src/jerk.py
+++ src/jerk.py
@@ -8,7 +8,6 @@ import math
 i2c = busio.I2C(board.SCL, board.SDA)
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
 prevAcceleration = accelerometer.acceleration
-stepNum = 0
 
 def mag(vector):
 	return math.sqrt(vector[0]**2+vector[1]**2+vector[2]**2)
@@ -23,16 +22,32 @@ def stepTaken(prevAcc,curAcc):
 	else:
 		return False
 
+class Jerk:
+	def __init__(self): #Defines the initial object parameters
+		self.stepNum = stepNum
+
+	def stepCount(self): #Returns the string for number of steps
+		return str("You've taken %i steps" %self.stepNum
+
+	def stepNum(self): #Returns the integer value for stepNum
+		return self.stepNum
+
+
+	def reset(self): #Resets the number of steps taken
+		self.stepNum = 0
 
 
 timeInc = 0.06
 timeBet = 0.3
-while True:
-	if(stepTaken(prevAcceleration,accelerometer.acceleration)):
-		stepNum = stepNum + 1
+
+## Uncomment when testing step tester ##
+
+#while True:
+#	if(stepTaken(prevAcceleration,accelerometer.acceleration)):
+#		stepNum = stepNum + 1
 #		print ("%f" %mag(accelerometer.acceleration))
 #		print ("%f" %mag(prevAcceleration))
-		print ("you took step %i" %stepNum)
-		time.sleep(timeBet)
-	prevAcceleration = accelerometer.acceleration
-	time.sleep(timeInc)
+#		print ("you took step %i" %stepNum)
+#		time.sleep(timeBet)
+#	prevAcceleration = accelerometer.acceleration
+#	time.sleep(timeInc)
