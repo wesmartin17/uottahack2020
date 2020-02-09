1cfb2954e8f8fcdc752b5192744f2024c1560600
Wes <wes.martin17@gmail.com>
2020-02-09 06:33:00 -0500

    idk anymore

--- src/clock_1.py
+++ src/clock_1.py
@@ -86,6 +86,7 @@ def demo(papirus):
 
         draw.text((5, 10), '{h:02d}:{m:02d}:{s:02d}'.format(
             h=now.hour, m=now.minute, s=now.second), fill=BLACK, font=clock_font)
+        draw.rectangle((10, 50, width-2, height-2), fill=WHITE)
         draw.text((10, 50), str(j.getJerk()))
 
         # display image on the panel
