24ef95b368b2a9d060ebff856c9dba8dc1e7c8d3
Wes <wes.martin17@gmail.com>
2020-02-09 06:15:32 -0500

    aaaa

--- src/jerk.py
+++ src/jerk.py
@@ -5,49 +5,45 @@ import adafruit_adxl34x
 import math
 
 
-i2c = busio.I2C(board.SCL, board.SDA)
-accelerometer = adafruit_adxl34x.ADXL345(i2c)
-prevAcceleration = accelerometer.acceleration
-
 def mag(vector):
-	return math.sqrt(vector[0]**2+vector[1]**2+vector[2]**2)
+    return math.sqrt(vector[0]**2+vector[1]**2+vector[2]**2)
 
 
 jerkMax = 3.6
-def stepTaken(prevAcc,curAcc):
-	if(mag(curAcc)-mag(prevAcc) > jerkMax):
-		return True
-	if(mag(curAcc)-mag(prevAcc) < -jerkMax):
-		return True
-	else:
-		return False
-
-class Jerk:
-	def __init__(self): #Defines the initial object parameters
-		self.stepNum = stepNum
-
-	def stepCount(self): #Returns the string for number of steps
-		return str("You've taken %i steps" %self.stepNum)
-
-	def stepNum(self): #Returns the integer value for stepNum
-		return self.stepNum
-
-
-	def reset(self): #Resets the number of steps taken
-		self.stepNum = 0
-
-
 timeInc = 0.06
 timeBet = 0.3
 
-## Uncomment when testing step tester ##
 
-#while True:
-#	if(stepTaken(prevAcceleration,accelerometer.acceleration)):
-#		stepNum = stepNum + 1
-#		print ("%f" %mag(accelerometer.acceleration))
-#		print ("%f" %mag(prevAcceleration))
-#		print ("you took step %i" %stepNum)
-#		time.sleep(timeBet)
-#	prevAcceleration = accelerometer.acceleration
-#	time.sleep(timeInc)
+class Jerk:
+    i2c = busio.I2C(board.SCL, board.SDA)
+    accelerometer = adafruit_adxl34x.ADXL345(i2c)
+    prevAcceleration = accelerometer.acceleration
+
+    def stepTaken(self, prevAcc, curAcc):
+        if(mag(curAcc)-mag(prevAcc) > jerkMax):
+            return True
+        if(mag(curAcc)-mag(prevAcc) < -jerkMax):
+            return True
+        else:
+            return False
+
+    def __init__(self):  # Defines the initial object parameters
+        self.stepNum = stepNum
+
+    def stepCount(self):  # Returns the string for number of steps
+        return str("You've taken %i steps" % self.stepNum)
+
+    def stepNum(self):  # Returns the integer value for stepNum
+        return self.stepNum
+
+    def reset(self):  # Resets the number of steps taken
+        self.stepNum = 0
+
+    def getJerk(self):
+        if(stepTaken(prevAcceleration, accelerometer.acceleration)):
+            stepNum = stepNum + 1
+            print("%f" % mag(accelerometer.acceleration))
+            print("%f" % mag(prevAcceleration))
+            print("you took step %i" % stepNum)
+        prevAcceleration = accelerometer.acceleration
+        return self.stepNum
