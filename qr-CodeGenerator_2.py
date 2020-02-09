8158e5dabf37f8b6fa90b778a304a913ce5ae87b
ErikCassidy <47614147+ErikCassidy@users.noreply.github.com>
2020-02-09 11:59:12 -0400

    Update qr-CodeGenerator_2.py

--- qr-CodeGenerator_2.py
+++ qr-CodeGenerator_2.py
@@ -2,7 +2,7 @@ import qrcode
 
 qr = qrcode.QRCode (version = 1, box_size = 15, border=5)
 
-data = "https://cdn-01.media-brady.com/store/stus/media/catalog/product/cache/4/image/85e4522595efc69f496374d01ef2bf13/1544626175/m/e/medical-alert-peanut-allergy-food-allergy-signs-l12150-lg.jpg"
+data = "https://bit.ly/2tGOxyb"
 qr.add_data(data)
 qr.make(fit = True)
 img = qr.make_image(fill = "black", back_color  = "white")
